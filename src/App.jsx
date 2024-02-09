f88881c6bd805ace66ca16a352e6ac03025b1530
juan sarmiento <juan.sarmiento@autodesk.com>
2024-02-09 10:20:19 -0500

    adding the tic tac toe app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,86 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Player from './components/Player'
+import GameBoard from './components/GameBoard'
+import Log from './components/Log'
+
+import { WINNING_COMBINATIONS } from './winning-combinations'
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
+const deriveActivePlayer = (gameTurns) => {
+  let currentPlayer = 'X';
+
+  if(gameTurns.length > 0  && gameTurns[0].player === 'X'){
+    currentPlayer = 'O'
+  }
+  return currentPlayer
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ gameTurns, setGameTurns ] = useState([])
+
+  const activePlayer = deriveActivePlayer(gameTurns)
+
+  let gameBoard = initialGameBoard
+
+  for(const turn of gameTurns){
+      const { square, player } = turn
+      const { row, col } = square
+
+      gameBoard[row][col] = player
+  }
+
+  let winner
+
+  for(const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol  = gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]  
+    const thirdSquareSymbol  = gameBoard[combination[2].row][combination[2].column] 
+
+    if (
+      firstSquareSymbol && 
+      firstSquareSymbol === secondSquareSymbol && 
+      firstSquareSymbol === thirdSquareSymbol
+      ){ 
+        winner = firstSquareSymbol
+      }
+  }
+
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    
+    setGameTurns(prevTurns => {
+      const currentPlayer = deriveActivePlayer(prevTurns)
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex}, player: currentPlayer }, 
+        ...prevTurns, 
+      ]
+
+      return updatedTurns
+
+    })
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div id="game-container">
+        <ol id="players" className='highlight-player'>
+          <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} />
+          <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
+        </ol>
+        { winner && <p>You won, {winner}!</p> }
+       <GameBoard  onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+      <Log turns={gameTurns} />
+    </main>
   )
 }
 
---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export default function Player({ initialName, symbol, isActive }){
+    const [playerName, setPlayerName ] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+
+    const handleEditing = () => setIsEditing(current => !current)
+
+    const handleNameChange = event => setPlayerName(event.target.value)
+
+    let playerNameField = isEditing ? 
+        <input 
+            placeholder="add a name" 
+            type="text" 
+            required 
+            value={playerName} 
+            onChange={handleNameChange}
+        /> : 
+        <span className="player-name">{playerName}</span>
+
+    return (
+        <li className={isActive ? 'active' : null }>
+            <span className="player">
+                {playerNameField}
+                <span className="player-symbol">{symbol}</span>
+                <button onClick={handleEditing} >{isEditing ? 'Save': 'Edit'}</button>
+            </span>
+        </li> 
+    )
+}
